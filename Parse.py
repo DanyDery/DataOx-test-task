745ec9ab0808f10bad7ea44d330c72ad66fcf692
Danylo <94636643+DanyDery@users.noreply.github.com>
2022-09-13 14:35:07 +0300

    Update Parse.py
    -remove PostgreSQL
    -create function for check date
    -implemented pagination content

--- Parse.py
+++ Parse.py
@@ -1,3 +1,4 @@
+from datetime import date, timedelta
 import pprint
 
 from dataclasses import dataclass, field
@@ -9,7 +10,7 @@ from sqlalchemy import Column, String, Integer, MetaData, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db_string = 'postgresql://dany:789654@localhost:5432/postgres'
+db_string = ''
 db = create_engine(db_string)
 base = declarative_base()
 
@@ -17,9 +18,8 @@ base = declarative_base()
 class Ad(base):
     __tablename__ = 'Ad'
 
-    id = Column("id", Integer, primary_key=True)
-    image = Column ("image", String(150))
-    title = Column("tittle", String(60))
+    image = Column("image", String(150))
+    title = Column("title", String(60), primary_key=True)
     date = Column("date", String(20))
     location = Column("location", String(20))
     bedrooms = Column("bedroom", String(10))
@@ -30,13 +30,10 @@ class Ad(base):
 Session = sessionmaker(db)
 session = Session()
 
-#  base.metadata.create_all(db) - don't work
-
 
 @dataclass
 class Product:
-
-    #id: int = field(init=False)
+    # id: int = field(init=False)
     image: str  # 1
     title: str  # 2
     date: str  # 3
@@ -87,20 +84,29 @@ class ParseConnect:
 
         self.preload_page_bs()
         div_for_info = self.bs.find_all('div', class_='info-container')
-        div_for_beds = self.bs.find_all('div', class_='rental-info')  # another div, where we can search amount of bedrooms
-        div_for_image = self.bs.find_all('div', class_='left-col')  # another div, where we can search link of images
+        div_for_beds = self.bs.find_all('div', class_='rental-info')  # div, where we can search amount of bedrooms
+        div_for_image = self.bs.find_all('div', class_='left-col')  # div, where we can search link of images
         return self.preload_info(div_for_info)
 
     def preload_info(self, product_cards: list[BeautifulSoup]):
         return [self.create_product(card) for card in product_cards]
 
+    def check_date(self, the_date: str):
+        if the_date == "Yesterday":
+            yesterday = date.today() - timedelta(days=1)
+            return yesterday.strftime("%d--%m--%Y")
+        elif "ago" in the_date:
+            return date.today().strftime("%d--%m--%Y")
+        else:
+            return the_date.replace('/', '--')
+
     @staticmethod
     def create_product(card):
-        image = card.find('div', class_='left-col')#.find('div', class_='image')
+        image = card.find('div', class_='image')#.find('img').get('data-src')
         title = card.find('div', class_='title').text.strip()
         date = card.find('div', class_='location').find('span', class_='date-posted').text.strip()
         location = card.find('div', class_='location').find('span', class_='').text.strip()
-        bedrooms = card.find('div', class_='rental-info')#.find('span', class_='bedrooms')#.text.strip().split()
+        bedrooms = card.find('span', class_='bedrooms')#.text.strip().split()
         description = card.find('div', class_='description').text.strip().split('\n')[0]
         price = card.find('div', class_='price').text.strip()
         return Product(image=image, title=title, date=date, location=location, bedrooms=bedrooms,
@@ -108,11 +114,21 @@ class ParseConnect:
 
 
 b = ParseConnect()
-а = b.get_data()
+lst = b.get_data()
+
+i = iter(b)
+
+# try:
+#     while True:
+#         next(i)
+#         lst.append(b.get_data())
+#         print(b.get_data())
+# except:
+#     pass
 
 
-# i = iter(b)
-# while True:
-#     next(i)
-
-
+# for prod in lst:
+#     ad = Ad(image=prod.image, title=prod.title, date=prod.date, location=prod.location, bedrooms=prod.bedrooms,
+#             description=prod.description, price=prod.price)
+#     session.add(ad)
+#     session.commit()
